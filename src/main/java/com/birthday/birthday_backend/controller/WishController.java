8135994246b1e0181b7248598ecd3449046a30f3
shrirangpatil007 <shrirangpatil007@gmail.com>
2025-07-04 17:17:23 +0530

    Update WishController.java

--- src/main/java/com/birthday/birthday_backend/controller/WishController.java
+++ src/main/java/com/birthday/birthday_backend/controller/WishController.java
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/wishes")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"https://birthday-frontend.netlify.app/"})
+@CrossOrigin(origins = "https://birthday-frontend.netlify.app")
 public class WishController {
 
     private final WishRepository wishRepository;
